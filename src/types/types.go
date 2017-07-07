ad8322a1ba306481af676d20ea9778b1376acbe6
Marie Antoinette Go <marie@plus65.com.ph>
2017-07-07 19:07:03 +0800

    add float declaration sample

--- src/types/types.go
+++ src/types/types.go
@@ -6,25 +6,34 @@ import "fmt" //sample library package usage, format
  * Multi line comment
  */
 
-var z int = 5
+var z int = 5 //global variable declaration
 
 func main()  {
-  //integer
-  fmt.Println("1 + z =", z + 1)
-
-  //floating
-  fmt.Println("1 + 1 =", 1.0 + 1.0)
-
-  var x = "Hello"
-  y:= "World"
-
-  fmt.Println(x + " "+ y)
-
+  //method call
+  sample_variable_declaration()
   sample_global_var()
   sample_input()
   sample_if()
   sample_for()
   sample_switch()
+}
+
+func sample_variable_declaration() {
+	var z float64 = 45.6
+
+	//integer
+	fmt.Println("1 + z =", z + 1)
+
+	//floating
+	fmt.Println("1 + 1 =", 1.0 + 1.0)
+
+	var x = "Hello"
+	y:= "World"
+
+	is_bool:= true
+
+	fmt.Println(x + " "+ y)
+	fmt.Println(is_bool)
 }
 
 func sample_global_var()  {
@@ -49,33 +58,33 @@ func sample_input() {
 }
 
 func sample_if() {
-	var input int
+	var value int
 	
 	fmt.Println("sample if")
 	
-	fmt.Scanf("%f",   &input)
+	fmt.Scanf("%f",   &value)
 
-	if (input % 2) == 0 {
-		fmt.Println(input, " is even")		
+	if (value % 2) == 0 {
+		fmt.Println(value, "is even")		
 	} else {
-		fmt.Println(input, " is odd")
+		fmt.Println(value, "is odd")
 	}		
 }
 
 
 func sample_for() {
-	var input int
+	var loopcount int
 	
 	fmt.Println("sample for")
 	
-	fmt.Scanf("%f", &input)
+	fmt.Scanf("%f", &loopcount)
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= loopcount; i++ {
 		fmt.Println(i)		
 	}
 
 	j:= 1
-	for j <= 10 {
+	for j <= 5 {
 		fmt.Println(j)
 		j = j + 1 			
 	}	
